715c8ed0e66344cfbe1d929e03c070a3ecee800f
vardhan <vardhanreddyg97@gmail.com>
2026-04-26 23:58:06 +0530

    Creating vnet and subnet

--- terraform-code/main.tf
+++ terraform-code/main.tf
@@ -4,3 +4,17 @@ resource "azurerm_resource_group" "rg" {
   name = "terraform-rg"
   location = "eastus2"
 }
+
+#Create a virtual network
+resource "azurerm_virtual_network" "vneta" {
+  name                =   "vnet01"
+  resource_group_name =   azurerm_resource_group.rg.name
+  location            =   azurerm_resource_group.rg.location
+  address_space       =   ["10.0.0.0/16"]
+
+
+  subnet {
+    name             = "subnet1"
+    address_prefixes = ["10.0.0.0/24"]
+  }
+}
